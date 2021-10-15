55dfb2d3c3e244d42f60adb17201cb59e4925200
Alex Overbeck <alexoverbeck@gmail.com>
2021-10-14 21:20:08 -0400

    WIPgs

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello World!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
