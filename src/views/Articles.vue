5bb05bcf6388866c979552cd32abf3ee4616d83e
Alex Overbeck <alexoverbeck@gmail.com>
2021-10-23 23:38:59 -0400

    Add Vue.js and route root url to Articles view

---
+++ src/views/Articles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Articles</h1>
+
+    <div v-if="loading">
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      articles: []
+    }
+  }
+}
+</script>
